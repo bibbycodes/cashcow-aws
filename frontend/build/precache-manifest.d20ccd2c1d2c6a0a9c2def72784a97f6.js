42d8c75f5e6642339030da905aee42091037e27e
Robert <bibbycodes@gmail.com>
2020-02-02 17:27:50 +0000

    added front end build to git

---
+++ frontend/build/precache-manifest.d20ccd2c1d2c6a0a9c2def72784a97f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2b5d18e734abf6fefda0d60d1ca677",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23c52137e33d7af00ed2",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d3fae21b0f46d546eba8",
+    "url": "/static/css/main.2d7bf9e0.chunk.css"
+  },
+  {
+    "revision": "23c52137e33d7af00ed2",
+    "url": "/static/js/2.5ba973c6.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.5ba973c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3fae21b0f46d546eba8",
+    "url": "/static/js/main.e5c213a2.chunk.js"
+  },
+  {
+    "revision": "8ffc8803dbe9ff207126",
+    "url": "/static/js/runtime-main.0c5ab20e.js"
+  }
+]);
